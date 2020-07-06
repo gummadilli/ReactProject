b484a622b29e43c3cc49166c22d2ebffdcfb5c29
Srinivasa Rao Gummadilli <SGummadilli@nationshearing.com>
2020-07-06 16:32:44 +0530

    Removed Code

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          welcome
-        </p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
